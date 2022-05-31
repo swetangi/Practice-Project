fa15426adbcb77f843d36ebc17ae10b38aa554f6
sweta <sweetysatasiya1610@gmail.com>
2022-05-31 11:25:47 +0530

    Node.js First Commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// console.log("Starting..");
+
+// setTimeout(() => {
+//     console.log("2 Second Timer");
+// }, 2000);
+// setTimeout(() => {
+//     console.log("0 Second Timer");
+// }, 0);
+// console.log("Stopping..");
+// const request=require('request');
+// const url="https://api.covid19api.com/summary";
+// request(
+//     {url : url},(error,response)=>{
+//         // console.log(response);
+//         // const data=JSON.parse(response.body);
+//         // console.log(data);
+      
+//         // console.log("The Cases are : "+ response.body.Global.TotalDeaths);
+//     }
+// )
+
